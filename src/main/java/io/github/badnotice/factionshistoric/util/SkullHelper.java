db3e22d9456d0f3a5b79d51d0af70c265d09054c
BADnotice <etiocook3388@gmail.com>
2021-11-10 12:42:41 -0300

    impl: SkullHelper.java

---
+++ src/main/java/io/github/badnotice/factionshistoric/util/SkullHelper.java
@@ -0,0 +1,65 @@
+package io.github.badnotice.factionshistoric.util;
+
+import com.google.common.collect.Maps;
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import org.bukkit.Material;
+import org.bukkit.SkullType;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.UUID;
+
+public final class SkullHelper {
+
+    private final static Map<String, ItemStack> HEAD_CACHE;
+
+    static {
+        HEAD_CACHE = Maps.newConcurrentMap();
+    }
+
+    public static ItemStack getTextureHead(String url) {
+        return HEAD_CACHE.computeIfAbsent(url, k -> createPlayerHead(url)).clone();
+    }
+
+    public static ItemStack getPlayerHead(Player player) {
+        return getPlayerHead(player.getName());
+    }
+
+    public static ItemStack getPlayerHead(String playerName){
+        return HEAD_CACHE.computeIfAbsent(playerName, k -> createPlayerHead(playerName)).clone();
+    }
+
+    private static ItemStack createPlayerHead(String playerName) {
+        ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.PLAYER.ordinal());
+        SkullMeta skullMeta = (SkullMeta)skull.getItemMeta();
+
+        skullMeta.setOwner(playerName);
+        skull.setItemMeta(skullMeta);
+
+        return skull;
+    }
+
+    private static ItemStack createTexture(String url) {
+        ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.PLAYER.ordinal());
+        SkullMeta skullMeta = (SkullMeta)skull.getItemMeta();
+
+        GameProfile gameProfile = new GameProfile(UUID.randomUUID(), null);
+        gameProfile.getProperties().put("textures", new Property("textures", url));
+
+        try{
+            Field gameProfileField = skullMeta.getClass().getDeclaredField("profile");
+            gameProfileField.setAccessible(true);
+            gameProfileField.set(skullMeta, gameProfile);
+        } catch (IllegalAccessException | NoSuchFieldException error) {
+            error.printStackTrace();
+        }
+
+        skull.setItemMeta(skullMeta);
+        return skull;
+    }
+
+}
--- src/main/java/io/github/badnotice/factionshistoric/view/PlayerHistoriesView.java
+++ src/main/java/io/github/badnotice/factionshistoric/view/PlayerHistoriesView.java
@@ -1,7 +1,6 @@
 package io.github.badnotice.factionshistoric.view;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import com.henryfabio.minecraft.inventoryapi.inventory.impl.paged.PagedInventory;
 import com.henryfabio.minecraft.inventoryapi.item.InventoryItem;
 import com.henryfabio.minecraft.inventoryapi.item.supplier.InventoryItemSupplier;
@@ -9,21 +8,14 @@ import com.henryfabio.minecraft.inventoryapi.viewer.configuration.impl.ViewerCon
 import com.henryfabio.minecraft.inventoryapi.viewer.impl.paged.PagedViewer;
 import io.github.badnotice.factionshistoric.player.PlayerHistoric;
 import io.github.badnotice.factionshistoric.util.ItemBuilder;
-import org.bukkit.Material;
-import org.bukkit.SkullType;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.SkullMeta;
+import io.github.badnotice.factionshistoric.util.SkullHelper;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public final class PlayerHistoriesView extends PagedInventory {
-
-    private final Map<String, ItemStack> HEAD_CACHE  = Maps.newConcurrentMap();
 
     public PlayerHistoriesView() {
         super(
@@ -54,12 +46,7 @@ public final class PlayerHistoriesView extends PagedInventory {
     }
 
     private InventoryItem playerHistoryInventoryItem(PlayerHistoric historic) {
-        ItemStack head = HEAD_CACHE.getOrDefault(
-                historic.getPlayerName(),
-                HEAD_CACHE.put(historic.getPlayerName(), getHeadPlayer(historic.getPlayerName()))
-        ).clone();
-
-        return InventoryItem.of(ItemBuilder.of(head)
+        return InventoryItem.of(ItemBuilder.of(SkullHelper.getPlayerHead(historic.getPlayerName()))
                 .displayName("§eHistorico de facção")
                 .lore(Arrays.asList(
                         "",
@@ -67,16 +54,6 @@ public final class PlayerHistoriesView extends PagedInventory {
                         " §fData: §7" + historic.getDateCreated()
                 ))
                 .getItemStack());
-    }
-
-    private ItemStack getHeadPlayer(String name) {
-        ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.PLAYER.ordinal());
-        SkullMeta skullMeta = (SkullMeta)skull.getItemMeta();
-
-        skullMeta.setOwner(name);
-        skull.setItemMeta(skullMeta);
-
-        return skull;
     }
 
 }
